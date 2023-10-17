2061c2bdc0045752f3ee8b35a40c256496f09288
sergiobni <bni.sergio@gmail.com>
2023-10-17 15:44:35 +0200

    tabbed component added

--- script.js
+++ script.js
@@ -68,3 +68,37 @@ btnScrollTo.addEventListener('click', function (e) {
   //Modern way of doing it
   secttion1.scrollIntoView({ behavior: 'smooth' });
 });
+
+//////////
+
+/////Tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+//bad practice, for each tab will be a copy, we need to use event delegation
+//tabs.forEach(t => t.addEventListener('click', () => console.log('tab')));
+
+//Using event delegation, attaching the addeventlistener to the individual element, instad all the the elements
+//Because there are more than 1 elements in the tab, we need to a way to get the whole tab selected when we click on the area, and not individual elements like span
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  //Guard clause, ignore any clicks when there are clicks in the area and result is null
+  if (!clicked) return; //if the click is inside the desidered target, continue
+
+  //Activate tab slide up
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  //Activate content area
+  //Removing the active content classses on all tabcontent
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  //Adding class active
+  //Getting the tab number clicked and then apply to that tab de active class
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
