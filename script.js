ae57283cd86a72f24b514871597b60ecb2abc713
sergiobni <bni.sergio@gmail.com>
2023-10-17 12:54:51 +0200

    page navigation, smooth scrolling and event delegation added

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 
 ///////////////////////////////////////
 // Modal window
-
+/////Selectors
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const secttion1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -29,4 +31,40 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+/////Events listeners
+
+/////Page navigation
+
+///Smooth scrolling with event delegation
+///Nav bar buttons
+//1.Add event listener to common parent element
+//2. Determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log(e.target);
+  e.preventDefault(); //Preventing the href html jump
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href'); //Getting the href html id
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+///Learn more button
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = secttion1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  // //Old school way of doing it
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageYOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  //Modern way of doing it
+  secttion1.scrollIntoView({ behavior: 'smooth' });
 });
